4f83eec525a697c3369b17742bf84b2320208786
pf4 <74263128+elydre@users.noreply.github.com>
2022-06-27 11:08:44 +0200

    0.2.8
    statut: dev

--- pusher.ps1
+++ pusher.ps1
@@ -3,5 +3,6 @@ git add -A
 git commit -m $version -m "statut: $((((cat .\version_history.md)[-1] -split " \| ")[-1] -split " ")[1])"
 git push
 git tag $version
+git tag latest
 git push --tags
 echo "Version $version deployed"
