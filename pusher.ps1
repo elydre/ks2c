5a25107333d9086a734ede730ceae755ba7fc53e
pf4 <74263128+elydre@users.noreply.github.com>
2022-06-27 11:19:17 +0200

    0.2.8
    statut: dev

--- pusher.ps1
+++ pusher.ps1
@@ -2,7 +2,13 @@ $version = (((cat .\version_history.md)[-1] -split "\| \[")[1] -split "\]")[0]
 git add -A
 git commit -m $version -m "statut: $((((cat .\version_history.md)[-1] -split " \| ")[-1] -split " ")[1])"
 git push
+
+if ($(git fetch --all --tags) -like "*$version*") {
+    echo "le tag $version existe, suppression..."
+    git tag -d $version
+    git push --delete origin $version
+}
 git tag $version
-git tag latest
 git push --tags
+
 echo "Version $version deployed"
