bfc0ff741455bac3edfc655d771fa0452b9a7d72
pf4 <74263128+elydre@users.noreply.github.com>
2022-06-27 07:46:20 +0200

    0.2.7

--- pusher.ps1
+++ pusher.ps1
@@ -28,8 +28,9 @@ $Button.Add_Click(
         {    
             $version = (((cat .\version_history.md)[-1] -split "\| \[")[1] -split "\]")[0]
             git add -A
-            git commit -m "$version"
+            git commit -m $version
             git push
+            $Button.Text = $version
         }
     )
 # ajout du bouton à la fenêtre
