47efd332ea6e3babdbe0b278c720c55ae7e0c92c
pf4 <74263128+elydre@users.noreply.github.com>
2022-06-27 07:43:31 +0200

    0.2.7

--- pusher.ps1
+++ pusher.ps1
@@ -9,28 +9,29 @@ $window.Height = 120
 $window.StartPosition = 'CenterScreen'
 $window.FormBorderStyle = 3 #fenêtre à taille fixe
 $window.BackColor = [System.Drawing.Color]::Gray
+$window.ForeColor = [System.Drawing.Color]::White
+$window.Font = New-Object System.Drawing.Font('Arial', 12)
+$window.TopMost = $true
 
 # bouton
 $Button = New-Object System.Windows.Forms.Button
 $Button.Text = 'commit version'
 
 # dimensions du bouton
-$Button.Width = 100
+$Button.Width = 140
 $Button.Height = 30
 
 # position du bouton
-$Button.Left = 50
+$Button.Left = 30
 $Button.Top = 25
 
 # action du bouton
 $Button.Add_Click(
         {    
             $version = (((cat .\version_history.md)[-1] -split "\| \[")[1] -split "\]")[0]
-            echo "version: $version"
             git add -A
             git commit -m "$version"
             git push
-            echo "version $version committed"
         }
     )
 # ajout du bouton à la fenêtre
